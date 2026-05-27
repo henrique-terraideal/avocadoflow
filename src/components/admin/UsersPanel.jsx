0268656aa47911ad942e4bf7a0a9af710b86d92c
Henrique Zaparoli Marques <henrique@terraideal.com.br>
2026-05-27 13:09:03 +0000

    File changes

--- src/components/admin/UsersPanel.jsx
+++ src/components/admin/UsersPanel.jsx
@@ -18,7 +18,10 @@ export default function UsersPanel() {
 
   const { data: users = [], isLoading: loadingUsers } = useQuery({
     queryKey: ["users-admin"],
-    queryFn: () => base44.entities.User.list(),
+    queryFn: async () => {
+      const res = await base44.functions.invoke("listUsers", {});
+      return res.data.users || [];
+    },
   });
 
   const { data: operators = [], isLoading: loadingOperators } = useQuery({
